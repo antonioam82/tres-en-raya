a84cf057544defb42ea00253e4b942a7d8ea8073
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-04-26 19:33:13 +0200

    Create juego_del_gato.py

---
+++ juego_del_gato.py
@@ -0,0 +1,16 @@
+#JUEGO DEL GATO
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog
+
+ventana=Tk()
+ventana.title("Juego del Gato")
+ventana.geometry("400x500")
+turno=0
+nombreJugador1=("")
+nombreJugador2=("")
+listaBotones=[]
+t=[]
+turnoJugador=StringVar()
+
+ventana.mainloop()
