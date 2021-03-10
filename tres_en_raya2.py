2b4c60c70dcb215de022e6f3686fe777bb7e0607
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-10 11:45:57 +0100

    Update tres_en_raya2.py

--- tres_en_raya2.py
+++ tres_en_raya2.py
@@ -4,13 +4,18 @@ import random
 import time
 from tkinter import simpledialog
 
+def change_btn(n):
+    botones[n].configure(bg="light blue")
+
+def bloqueo():
+    for i in botones:
+        i.configure(state='disabled')
 
 ventana=Tk()
 ventana.title("Tres en Raya")
 ventana.geometry("388x450")
-turno=0
 
-boton0=Button(ventana,width=9,height=3)
+boton0=Button(ventana,width=9,height=3,command=lambda: change_btn(0))
 boton0.place(x=50,y=50)
 boton1=Button(ventana,width=9,height=3)
 boton1.place(x=150,y=50)
@@ -30,5 +35,10 @@ boton8=Button(ventana,width=9,height=3)
 boton8.place(x=250,y=250)
 turnoE=Label(ventana).place(x=120,y=20)
 iniciar=Button(ventana,bg="dark blue",fg="white",text="Iniciar Juego",width=15,height=3).place(x=130,y=350)
+
+botones = [boton0,boton1,boton2,boton3,boton4,boton5,
+           boton6,boton7,boton8]
+
+#bloqueo()
 
 ventana.mainloop()
